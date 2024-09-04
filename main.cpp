a03cb83b41bbbcb4a9d3991acdeb95be9f7bf6e1
Eman Ashraf <162309520+E-emanAshraf@users.noreply.github.com>
2024-09-04 03:52:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "newsmanagementsystem.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    newsmanagementsystem w;
+    w.show();
+    return a.exec();
+}
